d9263b6ba53e607536597c62a1ae65e54c6405e0
Ekaterina <katepthv12@gmail.com>
2023-11-13 20:39:46 +0100

    feat: solve getLinearEquationRoot task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -115,8 +115,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scalar = x1 * x2 + y1 * y2;
+  const x = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const y = Math.sqrt(x2 ** 2 + y2 ** 2);
+
+  return Math.acos(scalar / (x * y));
 }
 
 /**
