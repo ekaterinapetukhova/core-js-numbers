4a7f43f82b67024ec5f3b9a969d3e928b19fcd34
Ekaterina <katepthv12@gmail.com>
2023-11-14 15:36:48 +0100

    feat: solve getFloatOnString task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -449,8 +449,8 @@ function isNumber(number) {
  * 5.1  => false
  * '5'  => false
  */
-function isInteger(/* number */) {
-  throw new Error('Not implemented');
+function isInteger(number) {
+  return Number.isInteger(number);
 }
 
 /**
@@ -463,8 +463,8 @@ function isInteger(/* number */) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  return Number.parseFloat(str);
 }
 
 /**
